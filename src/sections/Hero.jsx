fb795a363acd641ef7ae616776becd201eeec9ca
2shruti2 <shruthesharma@gmail.com>
2024-07-14 02:08:02 +0530

    prototype

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,33 @@
+import Navbar from "../components/Navbar";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div id="hero">
+      <Navbar />
+      <div className="w-screen h-screen bg-ruins max-sm:bg-right-bottom bg-cover bg-no-repeat flex max-sm:flex-col">
+        <div className="flex-1">
+          <h1 className="text-white text-6xl font-extrabold text-center mt-[40%]">
+            LEVEL UP WITH{" "}
+            <span className="sm:hover:text-7xl">
+              COSMO
+              <span className="text-primary">BYTE</span>
+            </span>
+          </h1>
+        </div>
+        <div className="flex-1">
+          <div className="my-28 mx-7 sm:mx-16 border border-primary hover:shadow-2xl hover:shadow-primary h-[60%] flex justify-start items-center rounded-lg p-2 sm:p-4">
+            <div className="h-full w-full rounded-lg backdrop-blur-md bg-primary">
+              <img
+                src="/assets/hero-headphone.png"
+                alt="headphone"
+                className="object-contain w-full h-full hover:scale-105"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
