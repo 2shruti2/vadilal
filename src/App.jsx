fb80f8ba116c8d19edd6e6c0302e580e93379cc0
2shruti2 <shruthesharma@gmail.com>
2024-07-13 14:07:09 +0530

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import WelcomeScreen from './components/WelcomeScreen'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <WelcomeScreen/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
