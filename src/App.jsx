e5ea076af79d95b557561c1cf6c8aed57afa2c3a
2shruti2 <shruthesharma@gmail.com>
2024-07-19 03:17:45 +0530

    vadilal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero";
+import MarqueeSlider from "./sections/MarqueeSlider";
 
 function App() {
   return (
     <>
       <Hero />
+      <MarqueeSlider/>
     </>
   );
 }
