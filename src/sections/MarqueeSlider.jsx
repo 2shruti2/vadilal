badd5fe0c8b605d121b80d55d55008eda0e2c7fd
2shruti2 <shruthesharma@gmail.com>
2024-07-20 01:07:53 +0530

    minor changes

--- src/sections/MarqueeSlider.jsx
+++ src/sections/MarqueeSlider.jsx
@@ -1,7 +1,89 @@
 const MarqueeSlider = () => {
   return (
-    <div className="w-full h-screen">MarqueeSlider</div>
-  )
-}
+    <div className="w-full h-screen">
+      <div
+        className="slider"
+        style="
+            --width: 100px;
+            --height: 50px;
+            --quantity: 10;
+        "
+      >
+        <div className="list">
+          <div className="item" style="--position: 1">
+            <img src="images/slider1_1.png" alt="" />
+          </div>
+          <div className="item" style="--position: 2">
+            <img src="images/slider1_2.png" alt="" />
+          </div>
+          <div className="item" style="--position: 3">
+            <img src="images/slider1_3.png" alt="" />
+          </div>
+          <div className="item" style="--position: 4">
+            <img src="images/slider1_4.png" alt="" />
+          </div>
+          <div className="item" style="--position: 5">
+            <img src="images/slider1_5.png" alt="" />
+          </div>
+          <div className="item" style="--position: 6">
+            <img src="images/slider1_6.png" alt="" />
+          </div>
+          <div className="item" style="--position: 7">
+            <img src="images/slider1_7.png" alt="" />
+          </div>
+          <div className="item" style="--position: 8">
+            <img src="images/slider1_8.png" alt="" />
+          </div>
+          <div className="item" style="--position: 9">
+            <img src="images/slider1_9.png" alt="" />
+          </div>
+          <div className="item" style="--position: 10">
+            <img src="images/slider1_10.png" alt="" />
+          </div>
+        </div>
+      </div>
 
-export default MarqueeSlider
+      <div
+        className="slider"
+        data-reverse="true"
+        style="
+            --width: 200px;
+            --height: 200px;
+            --quantity: 9;
+        "
+      >
+        <div className="list">
+          <div className="item" style="--position: 1">
+            <img src="images/slider2_1.png" alt="" />
+          </div>
+          <div className="item" style="--position: 2">
+            <img src="images/slider2_2.png" alt="" />
+          </div>
+          <div className="item" style="--position: 3">
+            <img src="images/slider2_3.png" alt="" />
+          </div>
+          <div className="item" style="--position: 4">
+            <img src="images/slider2_4.png" alt="" />
+          </div>
+          <div className="item" style="--position: 5">
+            <img src="images/slider2_5.png" alt="" />
+          </div>
+          <div className="item" style="--position: 6">
+            <img src="images/slider2_6.png" alt="" />
+          </div>
+          <div className="item" style="--position: 7">
+            <img src="images/slider2_7.png" alt="" />
+          </div>
+          <div className="item" style="--position: 8">
+            <img src="images/slider2_8.png" alt="" />
+          </div>
+          <div className="item" style="--position: 9">
+            <img src="images/slider2_9.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarqueeSlider;
