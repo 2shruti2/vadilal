a759418413e50969445d428ba45b5b59e9e97bac
2shruti2 <shruthesharma@gmail.com>
2024-07-21 05:12:03 +0530

    model added

---
+++ src/components/Model.jsx
@@ -0,0 +1,48 @@
+import { Canvas, useFrame  } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { useState, useEffect,  useRef, } from 'react';
+
+const ModelGlb = () => {
+  const { scene } = useGLTF('/pink-model.glb');
+
+  // scene.scale.set(0.4, 0.4, 0.4);
+  // scene.position.set(0, -1, 0);
+  scene.scale.set(3.5, 3.5, 3.5);
+  scene.position.set(0, -0.5, 0);
+  scene.rotation.set(
+    0, // Rotation around X-axis
+    0, // Rotation around Y-axis
+    Math.PI / 16  // Rotation around Z-axis
+  );
+
+  return <primitive object={scene} width="100vw" height="100vh" />;
+};
+
+const Model = () => {
+
+  return (
+    <Canvas className="canvas-container z-[-1] border-none" camera={{ position: [0, 0, 5], fov: 25 }}  >
+      <ambientLight intensity={2}/>
+
+      <pointLight position={[-3, 3, 3]} intensity={5} /> {/* Top left light */}
+      <pointLight position={[3, 3, 3]} intensity={5} /> {/* Top right light */}
+      <pointLight position={[-3, 3, -3]} intensity={5} /> {/* Top left back light */}
+      <pointLight position={[3, 3, -3]} intensity={5} /> {/* Top right back light */}
+
+      <pointLight position={[0, 5, 0]} intensity={10} /> {/* Top light */}
+      <pointLight position={[0, 1, 5]} intensity={10} /> {/* Front light */}
+      <pointLight position={[0, 1, -5]} intensity={10} /> {/* Back light */}
+
+      <pointLight position={[-5, 1, 0]} intensity={3} /> {/* Harsh left light */}
+      <pointLight position={[5, 1, 0]} intensity={3} /> {/* Harsh right light */}
+      
+      <ModelGlb  />
+      <OrbitControls 
+       enableZoom={false}
+       autoRotate
+      />
+    </Canvas>
+  );
+}
+
+export default Model;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,16 @@
+import Model from "../components/Model";
 import Navbar from "../components/Navbar";
 
 const Hero = () => {
   return (
-    <div id="hero" className="w-full h-screen gradient-sm sm:gradient max-sm:wave-sm sm:wave">
+    <div
+      id="hero"
+      className="w-full h-screen gradient-sm sm:gradient max-sm:wave-sm sm:wave relative"
+    >
       <Navbar />
+      <Model/>
+
+      <div className="z-30 top-1/2  absolute w-full h-full text-center flex justify-center items-center text-5xl text-white">Vadilal!</div>
     </div>
   );
 };
