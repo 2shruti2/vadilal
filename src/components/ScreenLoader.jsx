7af40ea1b3030aca9f4d97ff76c77a04780526df
2shruti2 <shruthesharma@gmail.com>
2024-07-24 06:29:44 +0530

    anim

--- src/components/ScreenLoader.jsx
+++ src/components/ScreenLoader.jsx
@@ -2,7 +2,7 @@
 
 const ScreenLoader = () => {
   return (
-    <div className="w-full h-screen bg-[#e674aa] absolute z-[100] top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+    <div className="w-full h-screen bg-yellow-100 absolute z-[100] top-0 left-0 right-0 bottom-0 flex justify-center items-center">
       <div>loading</div>
     </div>
   )
