9e68af06af5cab6d96486f5eb1aee258b978f998
2shruti2 <shruthesharma@gmail.com>
2024-07-24 05:32:44 +0530

    loader skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+import ScreenLoader from "./components/ScreenLoader";
 import Hero from "./sections/Hero";
 // import OurProduct from "./sections/our-products/OurProduct"
 import MarqueeSlider from "./sections/MarqueeSlider";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    };
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
   return (
     <>
-      <Hero />
-      <MarqueeSlider/>
-      {/* <OurProduct/> */}
+      {loading ? (
+        <ScreenLoader />
+      ) : (
+        <>
+          <Hero />
+          <MarqueeSlider />
+          {/* <OurProduct /> */}
+        </>
+      )}
     </>
   );
 }
